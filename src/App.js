5dcd5217aeffa2d2a0522f1d60f08ec958bef1ac
jesuscmdev <g.zuscm@gmail.com>
2024-01-11 19:10:48 -0600

    Quitamos blog

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Home from "./components/Home";
 import About from "./components/About";
 import Blog from "./components/Blog";
 import Contact from "./components/Contact";
-import Footer from './components/Footer'
+import Footer from "./components/Footer";
 import "./css/App.scss";
 
 function App() {
@@ -16,10 +16,10 @@ function App() {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/about" component={About} />
-        <Route exact path="/blog" component={Blog} />
+        {/* <Route exact path="/blog" component={Blog} /> */}
         <Route exact path="/contact" component={Contact} />
       </Switch>
-      <Footer/>
+      <Footer />
     </Router>
   );
 }
